60630238452c37e348236f55776571821140b2ca
Mendelt Siebenga <msiebenga@gmail.com>
2020-04-27 21:28:16 +0200

    add enums for writing commands

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,28 @@ where SPI: Write<u16, Error = E>
     // TODO send stuff to the DAC
 }
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u8)]
+enum Address {
+    DacA = 0b0000,
+    DacB = 0b0001,
+    DacC = 0b0010,
+    DacD = 0b0011,
+    DacE = 0b0100,
+    DacF = 0b0101,
+    DacG = 0b0110,
+    DacH = 0b0111,
+    AllDacs = 0b1111,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[repr(u8)]
+enum Command {
+    WriteInputRegister = 0b0000,
+    UpdateDacRegister = 0b0001,
+    WriteInputUpdateAll = 0b0010,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
