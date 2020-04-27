6e9a204aa84bcc84a73282bca93a699ac9d0bde0
Mendelt Siebenga <msiebenga@gmail.com>
2020-04-27 21:01:36 +0200

    setup driver struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+#![no_std]
+
+#![no_std]
+#[warn(missing_debug_implementations, missing_docs)]
+use embedded_hal::blocking::spi::Write;
+
+/// MCP4725 DAC driver. Wraps an I2C port to send commands to an MCP4725
+pub struct AD5668<SPI>
+{
+    spi: SPI,
+}
+
+impl<SPI, E> AD5668<SPI> 
+where SPI: Write<u16, Error = E>
+{
+    pub fn new(spi: SPI) -> Self {
+        Self { spi }
+    }
+
+
+
+    // TODO send stuff to the DAC
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
