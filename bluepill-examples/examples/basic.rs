f8eba902ba11eefed131f652b8ef4855fb5516e8
Mendelt Siebenga <msiebenga@gmail.com>
2020-05-17 16:33:55 +0200

    fix basic example to output a square wave

--- bluepill-examples/examples/basic.rs
+++ bluepill-examples/examples/basic.rs
@@ -5,7 +5,7 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use hal::spi::{Mode, Phase, Polarity, Spi};
+use hal::spi::{Mode, Phase, Polarity, Spi, NoMiso};
 use hal::pac;
 use hal::prelude::*;
 use hal::time::U32Ext;
@@ -27,11 +27,15 @@ fn main() -> ! {
     // let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
 
     // Configure the pins for SPI2
-    let pins = (
-        gpiob.pb13.into_alternate_push_pull(&mut gpiob.crh),
-        gpiob.pb14.into_floating_input(&mut gpiob.crh),
-        gpiob.pb15.into_alternate_push_pull(&mut gpiob.crh),
-    );
+    let spi2_mosi = gpiob.pb15.into_alternate_push_pull(&mut gpiob.crh);
+    let spi2_sck = gpiob.pb13.into_alternate_push_pull(&mut gpiob.crh);
+    let spi2_cs = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);
+
+    // let pins = (
+    //     gpiob.pb13.into_alternate_push_pull(&mut gpiob.crh),
+    //     gpiob.pb14.into_floating_input(&mut gpiob.crh),
+    //     gpiob.pb15.into_alternate_push_pull(&mut gpiob.crh),
+    // );
 
     // Configure SPI
     let spi_mode = Mode {
@@ -39,11 +43,12 @@ fn main() -> ! {
         phase: Phase::CaptureOnFirstTransition,
     };
 
-    let spi = Spi::spi2(dp.SPI2, pins, spi_mode, 100.khz(), clocks, &mut rcc.apb1);
-
-    let dac = AD5668::new(spi);
+    let spi = Spi::spi2(dp.SPI2, (spi2_sck, NoMiso, spi2_mosi), spi_mode, 100.khz(), clocks, &mut rcc.apb1);
+    let mut dac = AD5668::new(spi, spi2_cs);
 
     loop {
+        dac.write_and_update_dac_channel(Address::AllDacs, 0xffff).ok();
+        dac.write_and_update_dac_channel(Address::AllDacs, 0x0000).ok();
         // dac.send_stuff()
     }
 }
